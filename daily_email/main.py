164ed69aef2fe5447ea392049065517e9c50a655
ariannedee <ariannedee@gmail.com>
2022-02-22 17:33:08 -0800

    Handle api forbidden

--- daily_email/main.py
+++ daily_email/main.py
@@ -15,12 +15,29 @@ assert len(args) == 2, 'You must provide a "name" argument'
 name = args[1].strip()
 
 greeting = f"<h1>Good morning, {name.capitalize()} 👋</h1>"
-weather = f"<h2>Today's weather ☁️</h2><p>{get_weather_message()}</p>"
-joke = f"<h2>Joke of the day 🤣</h2><p>{get_joke()}</p>"
-quote = f"<h2>Quote of the day 💬</h2><p>{get_quote()}</p>"
-next_game = f"<h2>Next game 🏀</h2><p>{get_next_game()}</p>"
 
-content_list = [greeting, weather, joke, quote, next_game]
+content_list = [greeting]
+try:
+    weather = f"<h2>Today's weather ☁️</h2><p>{get_weather_message()}</p>"
+    content_list.append(weather)
+except Exception:
+    pass
+try:
+    joke = f"<h2>Joke of the day 🤣</h2><p>{get_joke()}</p>"
+    content_list.append(joke)
+except Exception:
+    pass
+try:
+    quote = f"<h2>Quote of the day 💬</h2><p>{get_quote()}</p>"
+    content_list.append(quote)
+except Exception:
+    pass
+try:
+    next_game = f"<h2>Next game 🏀</h2><p>{get_next_game()}</p>"
+    content_list.append(next_game)
+except Exception:
+    pass
+
 content = ''.join(content_list)
 content = content.replace('\n', '<br>')
 
