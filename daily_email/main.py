da3e19b639486ee16f10023e9ec4e2cda0d9bcf0
ariannedee <ariannedee@gmail.com>
2022-02-22 21:04:33 -0800

    Add error handling and success message

--- daily_email/main.py
+++ daily_email/main.py
@@ -17,28 +17,39 @@ name = args[1].strip()
 greeting = f"<h1>Good morning, {name.capitalize()} 👋</h1>"
 
 content_list = [greeting]
+errors = {}
 try:
     weather = f"<h2>Today's weather ☁️</h2><p>{get_weather_message()}</p>"
     content_list.append(weather)
-except Exception:
-    pass
+except OSError as e:
+    errors['weather'] = e
+
 try:
     joke = f"<h2>Joke of the day 🤣</h2><p>{get_joke()}</p>"
     content_list.append(joke)
-except Exception:
-    pass
+except OSError as e:
+    errors['joke'] = e
+
 try:
     quote = f"<h2>Quote of the day 💬</h2><p>{get_quote()}</p>"
     content_list.append(quote)
-except Exception:
-    pass
+except OSError as e:
+    errors['quote'] = e
+
 try:
     next_game = f"<h2>Next game 🏀</h2><p>{get_next_game()}</p>"
     content_list.append(next_game)
-except Exception:
-    pass
+except OSError as e:
+    errors['nba'] = e
 
 content = ''.join(content_list)
 content = content.replace('\n', '<br>')
 
+if errors:
+    for section, message in errors.items():
+        print(f'Error getting {section}: {message}')
+
 send_html_email(f"{name.capitalize()}'s daily email", content)
+
+print('Sent email:')
+print(content)
