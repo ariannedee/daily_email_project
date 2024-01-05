100742c0d0219e19b80079b138eab79c84a61dcf
ariannedee <ariannedee@gmail.com>
2024-01-04 16:35:18 -0800

    Revert changes from previous class

--- daily_email/apis/reminders/__init__.py
+++
@@ -1,1 +0,0 @@
-from .reminders import get_message
--- daily_email/apis/weather/test_weather.py
+++
@@ -1,9 +0,0 @@
-from .weather import c_to_f
-
-
-def test_0():
-    assert c_to_f(0) == 32
-
-
-def test_body_temp():
-    assert round(c_to_f(36.5)) == 98
