2139857ab279358562caf8d8ab23567951744a99
ariannedee <ariannedee@gmail.com>
2024-07-29 11:28:11 -0700

    Revert code from last class

--- daily_email/main.py
+++ daily_email/main.py
@@ -1,30 +1,3 @@
-import sys
-from pprint import pprint
+from send_email import send_text_email
 
-from apis.weather import get_message
-
-
-def main():
-    args = sys.argv
-    if len(args) > 1:
-        name = ' '.join(args[1:]).title()
-    else:
-        name = input('Name: ')
-
-    greeting = f"""Good morning, {name.strip().title()}
-
-{get_message()}
-
-Remember to:
-"""
-
-    with open('reminders.txt') as file:
-        for reminder in file.readlines():
-            greeting += f"- {reminder}"
-
-    print(greeting)
-    # send_text_email(subject='Daily email', content=greeting)
-
-
-if __name__ == '__main__':
-    main()
+send_text_email(subject='An email', content='This is a test email')
