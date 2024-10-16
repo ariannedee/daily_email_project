4a5d8b07c71e6bc7048380bb86afbe8ac32acca3
ariannedee <ariannedee@gmail.com>
2024-10-16 11:23:56 -0700

    Move weather to own module

---
+++ daily_email/apis/weather.py
@@ -0,0 +1,25 @@
+import requests
+
+from weather_codes import weather_from_code
+
+base_url = f'https://api.open-meteo.com/v1/forecast'
+params = {
+    'timezone': 'America/New_York',
+    'latitude': 42.997262156214305,
+    'longitude': -81.20390128320294,
+    'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset'],
+    'forecast_days': 1,
+}
+headers = {
+    'content-type': 'application/json'
+}
+response = requests.get(base_url, headers=headers, params=params)
+data = response.json()
+weathercode = data['daily']['weathercode'][0]
+weather = weather_from_code[weathercode]
+temp_c_high = data['daily']['temperature_2m_max'][0]
+temp_c_low = data['daily']['temperature_2m_min'][0]
+sunrise_str = data['daily']['sunrise'][0]
+sunset_str = data['daily']['sunset'][0]
+sunrise = sunrise_str.split('T')[1]
+sunset = sunset_str.split('T')[1]
--- daily_email/main.py
+++ daily_email/main.py
@@ -1,28 +1,14 @@
-from send_email import send_text_email
-
 import sys
 
-import requests
+from apis.weather import (
+    sunrise,
+    sunset,
+    temp_c_low as tcl,
+    temp_c_high as tch,
+    weather,
+)
+from send_email import send_text_email
 
-from weather_codes import weather_from_code
-
-base_url = f'https://api.open-meteo.com/v1/forecast'
-
-params = {
-    'timezone': 'America/New_York',
-    'latitude': 42.997262156214305,
-    'longitude': -81.20390128320294,
-    'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset'],
-    'forecast_days': 1,
-}
-
-headers = {
-    'content-type': 'application/json'
-}
-
-response = requests.get(base_url, headers=headers, params=params)
-
-data = response.json()
 
 def c_to_f(temp):
     return (temp * 9 / 5) + 32
@@ -40,25 +26,14 @@ if len(sys.argv) == 1:
 else:
     name = ' '.join(sys.argv[1:])
 
-weathercode = data['daily']['weathercode'][0]
-weather = weather_from_code[weathercode]
-temp_c_high = data['daily']['temperature_2m_max'][0]
-temp_c_low = data['daily']['temperature_2m_min'][0]
-
-sunrise_str = data['daily']['sunrise'][0]
-sunset_str = data['daily']['sunset'][0]
-
-sunrise = sunrise_str.split('T')[1]
-sunset = sunset_str.split('T')[1]
-
 with open('reminders.txt') as file:
     reminders = file.readlines()
 
 greeting = f"""Good morning, {name.title()}!
 Today will be {weather.lower()}.
 
-High of {temp_c_high:.0f} °C ({c_to_f(temp_c_high):.0f} °F)
-Low of {temp_c_low:.0f} °C ({c_to_f(temp_c_low):.0f} °F)
+High of {tch:.0f}°C ({c_to_f(tch):.0f}°F)
+Low of {tcl:.0f}°C ({c_to_f(tcl):.0f}°F)
 
 Sunrise: {sunrise}
 Sunset: {sunset}
