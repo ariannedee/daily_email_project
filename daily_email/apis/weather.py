751e9aae235e53a11496753b817f39713e5dadc5
ariannedee <ariannedee@gmail.com>
2025-08-26 12:20:52 -0700

    Test weather as separate script and raise custom exception

--- daily_email/apis/weather.py
+++ daily_email/apis/weather.py
@@ -1,9 +1,16 @@
 import requests
 
-from .weather_codes import weather_from_code
+try:
+    from .weather_codes import weather_from_code
+except ImportError:
+    from weather_codes import weather_from_code
 
 
 WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
+
+class APIError(Exception):
+    pass
+
 
 def get_weather_data(latitude, longitude):
     params = {
@@ -19,8 +26,7 @@ def get_weather_data(latitude, longitude):
     response = requests.get(WEATHER_URL, params, headers=headers)
 
     if response.status_code != 200:
-        # TODO: raise custom exception
-        raise Exception(f"Error getting weather data from {WEATHER_URL} with {params}\n{response.url}")
+        raise APIError(f"Error getting weather data from {WEATHER_URL} with {params}\n{response.url}")
 
     data = response.json()
     weather_code = data["daily"]["weather_code"][0]
@@ -30,4 +36,7 @@ def get_weather_data(latitude, longitude):
 
     return weather, temp_max, temp_min  # TODO: classes
 
-# TODO: run file as a script to test it
+
+if __name__ == "__main__":
+    weather, temp_high, temp_min = get_weather_data(0, 0)
+    print(f"{weather=}\n{temp_high=}\n{temp_min=}")
--- daily_email/main.py
+++ daily_email/main.py
@@ -2,7 +2,7 @@ import sys
 
 from environs import Env
 
-from daily_email.apis.weather import get_weather_data
+from apis.weather import get_weather_data
 from send_email import send_text_email
 
 env = Env()
@@ -12,40 +12,45 @@ env.read_env()
 def c_to_f(temp_c):
     return (temp_c * 9 / 5) + 32
 
+def main(name):
+    # Content greeting
+    content = f"Good morning, {name}!\n"
 
-name = " ".join(sys.argv[1:])
+    errors = []
 
-if not name:
-    name = input("Name: ")
+    lat = env.float('LATITUDE', default=49)
+    lon = env.float('LONGITUDE', default=-123)
 
-name = name.strip().title()
+    weather, temp_c_high, temp_c_low = get_weather_data(lat, lon)
 
-# Content greeting
-content = f"Good morning, {name}!\n"
-
-errors = []
-
-weather, temp_c_high, temp_c_low = get_weather_data(env.float('LATITUDE', default=49), env.float('LONGITUDE', default=-123))
-
-# Content weather
-# TODO: handle exceptions
-content += f"""\nToday is going to be {weather}.
-
+    # Content weather
+    # TODO: handle exceptions
+    content += f"""\nToday is going to be {weather}.
+    
 High: {temp_c_high :.0f}°C ({c_to_f(temp_c_high):.0f}°F)
 Low: {temp_c_low :.0f}°C ({c_to_f(temp_c_low):.0f}°F)
 \n"""
 
-# Content todos
-content += "Remember to:"
+    # Content todos
+    content += "Remember to:"
 
-with open("reminders.txt") as file:
-    for reminder in file.readlines():
-        content += "\n- " + reminder.strip().capitalize()
+    with open("reminders.txt") as file:
+        for reminder in file.readlines():
+            content += "\n- " + reminder.strip().capitalize()
 
-# Content errors
-if errors:
-    content += "\n\nERRORS -"
-    for error in errors:
-        content += f"\n[Error]: {error}"
+    # Content errors
+    if errors:
+        content += "\n\nERRORS -"
+        for error in errors:
+            content += f"\n[Error]: {error}"
 
-send_text_email(subject='An email', content=content)
+    send_text_email(subject='An email', content=content)
+
+if __name__ == '__main__':
+    receiver_name = " ".join(sys.argv[1:])
+
+    if not receiver_name:
+        receiver_name = input("Name: ")
+
+    receiver_name = receiver_name.strip().title()
+    main(receiver_name)
