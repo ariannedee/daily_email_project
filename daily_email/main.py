be738d1fc023f8f5f99f947ee480d920fbeac44a
ariannedee <ariannedee@gmail.com>
2026-07-28 10:57:52 -0700

    Move weather request to apis/weather.py

--- daily_email/main.py
+++ daily_email/main.py
@@ -1,12 +1,8 @@
 import sys
-from pprint import pprint
 
-import requests
-
+from apis.weather import temp_c_high, temp_c_low, weather
 from send_email import send_text_email
-from weather_codes import weather_from_code
 
-DEBUG = False
 
 def c_to_f(temp_c: int | float) -> float:
     return (temp_c * 9 / 5) + 32
@@ -17,47 +13,16 @@ if args:
 else:
     name = input("Name: ").strip()
 
-base_url = 'https://api.open-meteo.com/v1/forecast'
-
-params = {
-    'timezone': 'America/Los_Angeles',
-    'latitude': 49.2497,
-    'longitude': -123.1193,
-    'daily': ['weathercode', 'temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset'],
-    'forecast_days': 1,
-}
-
-headers = {
-    'content-type': 'application/json'
-}
-
-response = requests.get(base_url, params=params, headers=headers)
-
-data = response.json()
-
-if DEBUG:
-    pprint(data)
-
-today = data['daily']
-
-temp_c_high = today['temperature_2m_max'][0]
-temp_c_low = today['temperature_2m_min'][0]
-weathercode = today['weathercode'][0]
-weather = weather_from_code.get(weathercode, f"[unknown weather for code {weathercode}]")
-
-
 content = f"""Good morning, {name.title()}!
 
 Today is going to be {weather.lower()}.
-High: {temp_c_high:.0f}°C ({c_to_f(temp_c_high):.0f}°F)
-Low: {temp_c_low:.0f}°C ({c_to_f(temp_c_low):.0f}°F)
+High: {temp_c_high :.0f}°C ({c_to_f(temp_c_high):.0f}°F)
+Low: {temp_c_low :.0f}°C ({c_to_f(temp_c_low):.0f}°F)
 
 Remember to:
 """
 with open("reminders.txt") as file:
     for reminder in file.readlines():
         content += "- " + reminder
-
-print(content)
 
 send_text_email(subject='An email', content=content)
